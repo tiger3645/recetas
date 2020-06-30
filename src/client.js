1dcaee860d48fda4c3393fe8fde64d529096f17d
Emilio Mendez <emiliomendezcipriani@gmail.com>
2020-06-30 13:36:35 -0300

    Updates backend url

--- src/client.js
+++ src/client.js
@@ -1,11 +1,10 @@
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
-import { URL } from "./Constants";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: URL,
+  uri: "http://backend.emcipriani.com/",
 });
 
 const client = new ApolloClient({
